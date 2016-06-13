9dd330ce934ca9e7b97b7c30b86047a075a8f305
zjlester <zjlester@gmail.com>
2016-06-13 23:31:23 +0800

    test

---
+++ LMS_Authoring/ex_files/STEditor/sample.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt libero numquam tempore, laborum non quod! Deleniti similique saepe doloremque, est consequatur, quo qui ad dolorem ex ea atque, voluptatibus eos.</p>
+</body>
+</html>
